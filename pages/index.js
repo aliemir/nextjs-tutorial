5ac629c11c1fce0a4294104afe6c3914d5744911
Ali Emir Şen <ali.sen@softtech.com.tr>
2020-01-16 17:58:29 +0300

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
 import withLayout from '../components/MyLayout'
+import Link from 'next/link'
 
-const Index = () => <p>Hello Next.js</p>
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
-export default withLayout(Index)
+const Blog = () => {
+  return (
+    <>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title='Hello Next.js' />
+        <PostLink title='Learn Next.js is awesome' />
+        <PostLink title='Deploy apps with Zeit' />
+      </ul>
+    </>
+  )
+}
+
+export default withLayout(Blog)
